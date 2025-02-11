7b9902810eba66ad46419f8293e44883001978c2
Filonov Evgeniy <advent.alpen@gmail.com>
2025-02-11 06:43:08 +0300

    remake Views

--- DBConnection.php
+++
@@ -1,9 +0,0 @@
-<?php
-    const host = "localhost";
-    const user = "root";
-    const password = "1234";
-    const dbname = "chatDB";
-    $connect = mysqli_connect(host, user, password, dbname);
-    if(mysqli_connect_errno()) {
-        echo mysqli_connect_error();
-    }
