14679edda498c4a7f45344e3739ad9bb5b74a7c0
Filonov Evgeniy <advent.alpen@gmail.com>
2025-02-12 11:00:11 +0300

    change type to class

--- Controllers/exitAccount.php
+++ Controllers/exitAccount.php
@@ -1,5 +1,15 @@
 <?php
     namespace Chat\Controller;
-    session_destroy();
-    $new_page_url = 'http://localhost/chat/Views/loginPage.php';
-    header('Location: ' . $new_page_url);
+
+    $exitAccount = new ExitAccount();
+    $exitAccount->exitAccount();
+    
+    class ExitAccount
+    {
+        public function exitAccount()
+        {
+            session_destroy();
+            $new_page_url = 'http://localhost/chat/Views/loginPage.php';
+            header('Location: ' . $new_page_url);
+        }
+    }
